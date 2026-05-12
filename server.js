cc0640b06d88c2bfb50fc3b9efd854303058dca4
Roven <rovensantos03@gmail.com>
2026-05-12 09:42:43 +0800

    Configured some stuffs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3500;
+const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/dbNotes');
 const mongoose = require('mongoose');
 const cors = require('cors');
